d01719235bbc9168eb5ec266f31b6d28fcf44383
Kuphani <554934241@qq.com>
2020-08-17 16:19:22 +0800

    testbutton

---
+++ app/src/main/java/com/example/testbutton/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.testbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
